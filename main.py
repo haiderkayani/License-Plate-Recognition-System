9f09e97ea6983826681e072852e61d75608a4580
Haider <haidabid@gmail.com>
2023-12-20 03:26:42 +0500

    added images to dataset

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import imutils
 import easyocr
 
 # READ IMAGE AND CONVERT TO GRAYSCALE AND THEN BLUR ----------------------
-image = cv2.imread('images/image1.jpg')
+image = cv2.imread("images/1.jpg")
 grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 plt.imshow(grayscale_image, cmap='gray')
+plt.title('Grayscale image')
 # plt.axis('off')
 plt.show()
 
@@ -17,6 +18,7 @@ plt.show()
 bilateral_filter = cv2.bilateralFilter(grayscale_image, 11, 17, 17)
 edged_image = cv2.Canny(bilateral_filter, 30, 200)
 plt.imshow(cv2.cvtColor(edged_image, cv2.COLOR_BGR2RGB))
+plt.title('Image after canny edge detection')
 plt.show()
 
 # FINDING CONTOUR COORDINATES TO DETECT NUMBERPLATE --------------------------
@@ -38,6 +40,7 @@ mask =np.zeros(grayscale_image.shape, np.uint8)
 new_image = cv2.drawContours(mask, [location], 0, 255, -1)
 new_image = cv2.bitwise_and(image, image, mask=mask)
 plt.imshow(cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB))
+plt.title('Masked image')
 plt.show()
 
 (x,y) = np.where(mask==255)
@@ -45,6 +48,7 @@ plt.show()
 (x2,y2) = (np.max(x), np.max(y))
 cropped_image = grayscale_image[x1:x2+1, y1:y2+1]
 plt.imshow(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
+plt.title('Cropped image')
 plt.show()
 
 # USING OCR READER TO READ TEXT FROM CROPPED IMAGE ------------------------
@@ -57,6 +61,8 @@ result = reader.readtext(cropped_image)
 text = result[0][-2]
 font = cv2.FONT_HERSHEY_SIMPLEX
 final_text = cv2.putText(image, text=text, org=(approx[0][0][0], approx[1][0][1]+60), fontFace=font, fontScale=1, color=(0,255,0), thickness=2, lineType=cv2.LINE_AA)
-final_text = cv2.rectangle(image, tuple(approx[0][0]), tuple(approx[2][0]), (0,255,0),3)
+final_text = cv2.rectangle(image, tuple(approx[0][0]), tuple(approx[2][0]), (255,0,0),3)
 plt.imshow(cv2.cvtColor(final_text, cv2.COLOR_BGR2RGB))
+plt.title('Final image with license plate detection')
 plt.show()
+print('License Plate:', text)
